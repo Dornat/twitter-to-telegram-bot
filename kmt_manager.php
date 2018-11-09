ca2bad5926f5787542d88e0c3d4969c47c5b2596
Dornat <DjDornat1@mail.ru>
2018-11-09 12:11:27 +0200

    added const for kyivmetroalertsid

--- kmt_manager.php
+++ kmt_manager.php
@@ -5,6 +5,8 @@ require_once 'vendor/autoload.php';
 use Spatie\TwitterStreamingApi\PublicStream;
 use App\TweetManager;
 
+const KYIV_METRO_ALERTS_ID = '2199708446';
+
 $tweetManager = new TweetManager();
 
 PublicStream::create(
@@ -12,7 +14,7 @@ PublicStream::create(
     $accessTokenSecret,
     $consumerKey,
     $consumerSecret
-)->whenTweets('2199708446', function(array $tweet) use ($tweetManager) {
+)->whenTweets(KYIV_METRO_ALERTS_ID, function(array $tweet) use ($tweetManager) {
     $tweetManager->manage($tweet);
     echo json_encode($tweet, JSON_PRETTY_PRINT);
 })->startListening();
